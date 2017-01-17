9139d665e28d7a3f257a8a4716f7e82b6d2f2653
vinuthottian <vinuthottian@gmail.com>
2017-01-17 20:25:28 +0530

    commit

--- JavaApplication4/src/javaapplication4/IllitrateMen.java
+++ JavaApplication4/src/javaapplication4/IllitrateMen.java
@@ -10,5 +10,11 @@ package javaapplication4;
  * @author user
  */
 public class IllitrateMen {
+    public static void main(String[]args){
+        float p1 = (float)((80000*52)/100);
+        float p2 = (float)((p1*35)/100);
+    System.out.println("No of men in 80000 people are"+p1);
+    System.out.println("No of men in 80000 people are"+p2);
+    }
     
 }
